623eff11244963dc02093e993a3d6cad11f139fb
Mathieu Dolmen <mathieu.dolmen@gmail.com>
2020-10-01 19:06:31 +0200

    tests: fix a confusion in results comparison

--- tests/simulate_traffic.sh
+++ tests/simulate_traffic.sh
@@ -49,10 +49,11 @@ sleep 5
 echo "[+] Simulating traffic..."
 # IPV4 TCP
 ip netns exec $ns iperf3 -s -B 10.0.10.100 -1 &> /dev/null &
-sleep 1
-ip netns exec $ns iperf3 -4 -c 10.0.10.100 -p 5201 -n 2G -B 10.0.10.200 -J > $output1
+sleep 2
+ip netns exec $ns iperf3 -4 -c 10.0.10.100 -p 5201 -n 1G -B 10.0.10.200 -J > $output1
 sleep 5
 kill -s SIGINT $pid
+sleep 1
 
 # TODO: IPV4 UDP
 # TODO: IPV6 TCP
@@ -60,14 +61,18 @@ kill -s SIGINT $pid
 
 rx_intercepted=$(cat $output0 | jq '.rx')
 tx_intercepted=$(cat $output0 | jq '.tx')
-rx_iperf=$(cat $output1 | jq '.end.sum_sent.bytes')
-tx_iperf=$(cat $output1 | jq '.end.sum_received.bytes')
+rx_iperf=$(cat $output1 | jq '.end.sum_received.bytes')
+tx_iperf=$(cat $output1 | jq '.end.sum_sent.bytes')
 
 if [ $rx_intercepted == $rx_iperf ] && [ $tx_intercepted == $tx_iperf ]
 then
 	echo -e "[test] ${GREEN}IPV4 TCP: OK${NC}"
 else
 	echo -e "[test] ${RED}IPV4 TCP: iperf3 traffic != traffic intercepted${NC}"
+	echo "$rx_intercepted"
+	echo "$rx_iperf"
+	echo "$tx_intercepted"
+	echo "$tx_iperf"
 fi
 
 #
